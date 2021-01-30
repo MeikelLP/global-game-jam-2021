d069eb58d48ab035c6ef73f8caeb3240c0c0e260
Meikel Philipp <MeikelLP@users.noreply.github.com>
2021-01-30 23:12:49 +0100

    Add background scoller IMP camera

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private float speed;
+    
+    private SpriteRenderer _renderer;
+    private Transform _t;
+    
+
+    private void Start()
+    {
+        _t = transform;
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void LateUpdate()
+    {
+        var camPos = cam.transform.position;
+        camPos.z = 0;
+        _t.position = camPos + camPos * speed;
+    }
+}
